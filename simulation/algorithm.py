cb3d70004517d9f5837df3ab0e43fcf541286ad0
Shengying Pan <i@thepans.me>
2015-08-04 23:43:11 -0400

    updated top k algorithm

--- simulation/algorithm.py
+++ simulation/algorithm.py
@@ -8,6 +8,7 @@ def findAvailableWorkers(workers):
 			availables.append(workers[i])
 	return availables
 
+def findTopKAvailableWorkers(workers, observedCorrectness, k):
 
 
 
@@ -33,7 +34,9 @@ def pickRandomly(tasks, workers, limit):
 	return answers
 
 def pickTopK(tasks, workers, k, tutorials):
-	observedCorrectness = []
+	answers = []
+	observedCorrectness = {} #note here observed correctness may not be the actual correctness
+	#other than the tutorial, the other are just benchmarked from the aggregated avg answer
 	for i in range(0, len(workers)):
 		worker = workers[i]
 		count = 0
@@ -42,7 +45,34 @@ def pickTopK(tasks, workers, k, tutorials):
 			answer = worker.doTask(task)
 			if answer == task:
 				count += 1
-		observedQualities.append(count) #initialize observed quality
+		observedCorrectness[worker.uuid] = count
 
 	for i in range(0, len(tasks)):
 		task = tasks[i]
+		top = findTopKAvailableWorkers(workers, observedCorrectness, k)
+		currentAnswers = []
+		flag = 0
+		for j in range(0, len(top)):
+			currentAnswer = top[j].doTask(task)
+			currentAnswers.append(currentAnswer)
+			if currentAnswer:
+				flag += 1
+			else:
+				flag -= 1
+		if flag >= 0:
+			aggregatedAnswer = True
+		else:
+			aggregatedAnswer = False
+		answers.append(aggregatedAnswer)
+		for j in range(0, len(top)):
+			if currentAnswers[j] == aggregatedAnswer:
+				observedCorrectness[top[j].uuid] += 1
+
+	return answers
+
+
+
+
+
+
+
