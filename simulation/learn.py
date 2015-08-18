e970a02368a22936462fe34b9e55e6a826ffb7ed
Shengying Pan <span@teebie.com>
2015-08-17 20:01:15 -0400

    update

--- simulation/learn.py
+++ simulation/learn.py
@@ -1,7 +1,14 @@
+from scipy import stats
+
 #learning module
-def learnCurve(points):
-	#use regression to learn the hyperbolic learning curve
-	#the curve is y(x) = (x + p) / (x + p + r) p is the initial quality and r is the learning rate
-	#linear regression to learn the linearly transformed curve first
-	#then convert back to the hyperbolic form
-	return
+def learnCurve(cs, ts):
+	#cs is a list of number of correct answers
+	#ts is a list of total number of tasks
+	qs = cs / ts
+	zs = 1 / (1 - qs)
+
+	slope, intercept, r_value, p_value, std_err = stats.linregress(ts, zs)
+
+	result = {}
+
+	return result
---
+++ simulation/regtest.py
@@ -0,0 +1,10 @@
+import sys
+import numpy as numpy
+import matplotlib.pyplot as plot
+import simulate
+import learn
+
+
+if __name__ == '__main__':
+	print 'Regression Test'
+
