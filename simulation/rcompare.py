27fb51ccb452d3d9acc502480c66cc9db248ce11
Shengying Pan <i@thepans.me>
2015-08-28 10:19:59 -0400

    update

--- simulation/rcompare.py
+++ simulation/rcompare.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plot
 import simulate
 import learn
 import random
+import math
 from worker import Worker
 
 if __name__ == '__main__':
 	p = 0
 	r_start = 1
-	r_end = 100
+	r_end = 200
 	runs = 100
-	horizon = 100
+	horizon = 200
 	threshold = 0.01
 
 	tasks = simulate.createBinaryTasks(horizon)
@@ -34,10 +35,12 @@ if __name__ == '__main__':
 					count += 1
 				ts.append(j + 1)
 				cs.append(count)
-				if j < 5:
-					continue
 				learning = learn.learnCurve(cs, ts)
 				err = learning['e']
+				#print err
+				#print learning['r']
+				if math.isnan(err):
+					continue
 				if err < threshold:
 					total += j + 1
 					if j + 1 > maximum:
@@ -49,7 +52,13 @@ if __name__ == '__main__':
 	xs = np.arange(r_start, r_end + 1, 1)
 
 	f, (ax, bx) = plot.subplots(1, 2)
+	ax.set_title('Averaged across 100 runs')
+	ax.set_xlabel('r')
+	ax.set_ylabel('tasks')
 	ax.plot(xs, ys1)
+	bx.set_title('Maximum  in 100 Runs')
+	bx.set_xlabel('r')
+	bx.set_ylabel('tasks')
 	bx.plot(xs, ys2)
 	plot.show()
 
