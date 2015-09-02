53d0142bc7dd8336c967e59bb4384cb088c13f43
Shengying Pan <i@thepans.me>
2015-09-02 13:24:18 -0400

    ha

--- simulation/system.py
+++ simulation/system.py
@@ -6,7 +6,10 @@ class State:
 	worker = None
 	parent = None
 	children = []
+	answers = []
+	hirings = []
 	def __init__(self, parent, V, W):
+		self.parent = parent
 		self.V = V
 		self.W = W
 
@@ -17,16 +20,57 @@ class System:
 	root: State()
 	def __init__(self, start):
 		self.start = start
-	def sample(self, samples, horizon, task, workers):
+	def pickOutcome(self, outcomes, probabilities):
+		levels = []
+		total = 0
+		for o in range(0, len(outcomes)):
+			total += probabilities[o]
+			levels.append(total)
+		r = random.random()
+		for l in range(0, len(levels)):
+			level = levels[l]
+			if r < level:
+				return outcomes[l]
+		return None
+	def sample(self, samples, horizon, task, outcomes, workers):
 		for i in range(0, samples):
 			cursor = root
 			hired = 0
 			rank = 0
 			while hired < horizon:
-				worker = workers[rank]
+				worker = workers[rank] #next worker to hire
 				rank += 1
+				probabilities = []
+				sum_prob = 0
+				for o in range(0, len(outcomes)):
+					outcome = outcomes[o]
+					p1 = 1.0
+					p2 = 1.0
+					p3 = 1.0
+					for t in range(0, len(outcomes)):
+						truth = outcomes[t]
+						if outcome == truth:
+							p1 = worker.getQuality()
+						else:
+							p1 = 1 - worker.getQuality()
+						for a in range(0, len(cursor.answers)):
+							answer = cursor.answers[a]
+							hire = cursor.hirings[a]
+							if answer == truth:
+								p2 = p2 * hire.getQuality()
+							else:
+								p2 = p2 * (1 - hire.getQuality())
+						p3 = float(counts[truth]) / float(total)
+					probabilities.append(p1 * p2 * p3)
+					sum_prob += p1 * p2 * p3
+				#need to normalize probability
+				for o in range(0, len(outcomes)):
+					outcome = outcomes[o]
+					probabilities[0] = probabilities[o] / sum_prob
+				pick = self.pickOutcome(outcomes, probabilities)
 
-
+				nextState = State(cursor, 0, None)
+				
 
 
 
