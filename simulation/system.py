ce63825ca7537f66bdc8d9915f23d78515bcdd33
Shengying Pan <i@thepans.me>
2015-09-02 12:48:12 -0400

    ha

--- simulation/system.py
+++ simulation/system.py
@@ -1,12 +1,38 @@
+import random
+
 class State:
-	V = 0
-	W = None
-	def __init__(self, V, W):
+	utility = 0
+	visitation = 0
+	worker = None
+	parent = None
+	children = []
+	def __init__(self, parent, V, W):
 		self.V = V
 		self.W = W
 
 class System:
 	counts: {}
+	total = 0
+	start = 0
 	root: State()
-	def sample(self, samples, horizon):
+	def __init__(self, start):
+		self.start = start
+	def sample(self, samples, horizon, task, workers):
+		for i in range(0, samples):
+			cursor = root
+			hired = 0
+			rank = 0
+			while hired < horizon:
+				worker = workers[rank]
+				rank += 1
 
+
+
+
+
+
+				hired += 1
+
+	def evaluate(self):
+
+	def hire(self):
