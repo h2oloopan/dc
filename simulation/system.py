e7c174fa48be2cf301103dcac71737fe6c7d9c91
Shengying Pan <i@thepans.me>
2015-09-02 22:35:29 -0400

    update system

--- simulation/system.py
+++ simulation/system.py
@@ -5,7 +5,7 @@ class State:
 	visitation = 0
 	worker = None
 	parent = None
-	children = []
+	children = {}
 	answers = []
 	hirings = []
 	def __init__(self, parent, V, W):
@@ -20,6 +20,7 @@ class System:
 	root: State()
 	def __init__(self, start):
 		self.start = start
+		self.root.visitation = 1
 	def pickOutcome(self, outcomes, probabilities):
 		levels = []
 		total = 0
@@ -36,49 +37,54 @@ class System:
 		for i in range(0, samples):
 			cursor = root
 			hired = 0
-			rank = 0
+			#rank = 0
 			while hired < horizon:
-				worker = workers[rank] #next worker to hire
-				rank += 1
-				probabilities = []
-				sum_prob = 0
-				for o in range(0, len(outcomes)):
-					outcome = outcomes[o]
-					p1 = 1.0
-					p2 = 1.0
-					p3 = 1.0
-					for t in range(0, len(outcomes)):
-						truth = outcomes[t]
-						if outcome == truth:
-							p1 = worker.getQuality()
-						else:
-							p1 = 1 - worker.getQuality()
-						for a in range(0, len(cursor.answers)):
-							answer = cursor.answers[a]
-							hire = cursor.hirings[a]
-							if answer == truth:
-								p2 = p2 * hire.getQuality()
+				#worker = workers[rank] #next worker to hire
+				#ank += 1
+				for w in range(0, len(workers)):
+					worker = workers[w]
+					probabilities = []
+					sum_prob = 0
+					for o in range(0, len(outcomes)):
+						outcome = outcomes[o]
+						p1 = 1.0
+						p2 = 1.0
+						p3 = 1.0
+						for t in range(0, len(outcomes)):
+							truth = outcomes[t]
+							if outcome == truth:
+								p1 = worker.getQuality()
 							else:
-								p2 = p2 * (1 - hire.getQuality())
-						p3 = float(counts[truth]) / float(total)
-					probabilities.append(p1 * p2 * p3)
-					sum_prob += p1 * p2 * p3
-				#need to normalize probability
-				for o in range(0, len(outcomes)):
-					outcome = outcomes[o]
-					probabilities[0] = probabilities[o] / sum_prob
-				pick = self.pickOutcome(outcomes, probabilities)
+								p1 = 1 - worker.getQuality()
+							for a in range(0, len(cursor.answers)):
+								answer = cursor.answers[a]
+								hire = cursor.hirings[a]
+								if answer == truth:
+									p2 = p2 * hire.getQuality()
+								else:
+									p2 = p2 * (1 - hire.getQuality())
+							p3 = float(counts[truth]) / float(total)
+						probabilities.append(p1 * p2 * p3)
+						sum_prob += p1 * p2 * p3
+					#need to normalize probability
+					for o in range(0, len(outcomes)):
+						outcome = outcomes[o]
+						probabilities[0] = probabilities[o] / sum_prob
+					pick = self.pickOutcome(outcomes, probabilities)
 
-				nextState = State(cursor, 0, None)
-				nextState.visitation += 1
-				nextState.worker = worker
-				nextState.answers = list(cursor.answers)
-				nextState.answers.append(worker.doTask(task))
-				nextState.hirings = list(cursor.hirings)
-				nextState.hirings.append(worker)
-				cursor.children.append(nextState)
-				cursor = nextState
-				hired += 1
+					key = str(worker.uuid) + '.' + str(pick)
+					nextState = cursor.children[key]
+					if nextState == None:
+						nextState = State(cursor, 0, None)
+						nextState.worker = worker
+						nextState.answers = list(cursor.answers)
+						nextState.answers.append(worker.doTask(task))
+						nextState.hirings = list(cursor.hirings)
+						nextState.hirings.append(worker)
+						cursor.children[key] = nextState
+					nextState.visitation += 1
+					cursor = nextState
+					hired += 1
 
 	def evaluate(self):
 
