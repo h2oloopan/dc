080a116cf9ae54be2f510ba297d0fa6e0ab54c5a
Shengying Pan <span@teebie.com>
2015-11-12 14:36:45 -0500

    ha

--- data/analyze.py
+++ data/analyze.py
@@ -1,6 +1,47 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plot
-import learn
-from worker import Worker
+import os
+import sys
 
+sys.path.append('../simulation')
+from worker import Worker
+import learn
+
+expert = []
+workers = []
+
+
+def readData(fname):
+	result = []
+	with open(fname) as csvFile:
+		reader = csv.DictReader(csvFile)
+		for row in reader:
+			pair = [float(row['start']), float(row['end'])]
+			result.append(pair)
+	return result
+
+def overlap(p1, p2):
+	if p1[0] < p2[0] and p2[0] < p1[1]:
+		return True
+	elif p1[0] < p2[1] and p2[1] < p1[1]:
+		return True
+	elif p2[0] < p1[0] and p1[1] < p2[1]:
+		return True
+	else:
+		return False
+
+
+
+#do actual work
+for f in os.listdir('.'):
+	if f.endswith('.csv'):
+		if 'expert' not in f:
+			worker = readData(f)
+			workers.append(worker)
+		else:
+			expert = readData(f)
+
+print expert
+
+print workers[0]
