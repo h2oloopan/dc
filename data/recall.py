51350ade305bf638617345c86a7f881888e62706
Shengying Pan <span@teebie.com>
2015-11-19 21:04:24 -0500

    start working on fscore

--- data/recall.py
+++ data/recall.py
@@ -50,36 +50,28 @@ for f in os.listdir('.'):
 cursor = 0
 step = 20
 end = 1080
-precisions = []
+recalls = []
 xs = []
 correct = 0
 total = 0
 cumulative_precision = 0.0
 x = 0
+total_truth = 0
 while cursor < end:
 	range_start = cursor
 	range_end = cursor + step
 	cur_correct = 0
 	cur_total = 0
-	for worker in workers:
-		for spindle in worker:
-			if range_start <= spindle[0] and spindle[1] < range_end:
-				for truth in expert:
+	for truth in expert:
+		if range_start <= truth[0] and truth[1] < range_end:
+			total_truth += 1
+			for worker in workers:
+				for spindle in worker:
 					if overlap(truth, spindle):
 						correct += 1
-						cur_correct += 1
-						break
-				total += 1
-				cur_total += 1
 
-	#cur_precision = float(cur_correct) / float(cur_total)
-	#cumulative_precision = (float(x) * cumulative_precision + cur_precision) / float(x + 1)
+	recalls.append(float(correct) / float(len(workers) * total_truth))
 
-	if total == 0:
-		precisions.append(0.0)
-	else:
-		precisions.append(float(correct) / float(total))
-	
 	cursor += step
 	x += 1
 	xs.append(x)
@@ -87,7 +79,7 @@ while cursor < end:
 
 
 xs = xs[10:]
-ys = precisions[10:]
+ys = recalls[10:]
 zs = []
 for y in ys:
 	zs.append(1.0 / (1.0 - float(y)))
@@ -105,11 +97,11 @@ for x in xs:
 	curve.append(float(x + p) / float(x + p + r))
 
 
-plot.plot(xs, ys, label='actual precision')
-plot.plot(xs, curve, label='estimated precision')
+plot.plot(xs, ys, label='actual recall')
+plot.plot(xs, curve, label='estimated recall')
 plot.legend(bbox_to_anchor=(1, 0.3))
 plot.xlabel('windows')
-plot.ylabel('cumulative precision')
+plot.ylabel('cumulative recall')
 plot.show()
 
 
