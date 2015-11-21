e42961c30e90b5df239117621f8baf56f5b540a5
Shengying Pan <i@thepans.me>
2015-11-20 23:10:45 -0500

    ha

--- data/individual.py
+++ data/individual.py
@@ -53,10 +53,16 @@ for worker in workers:
 	xs = []
 	precisions = []
 	recalls = []
+	fscores = []
 	cursor = 0
 	step = 20
 	end = 1080
 	x = 0
+
+	c_precision = 0
+	c_recall = 0
+	t_precision = 0
+	t_recall = 0
 
 	while cursor < end:
 		range_start = cursor
@@ -64,20 +70,68 @@ for worker in workers:
 
 		#precision
 		for spindle in worker:
-			if range
-
+			if range_start <= spindle[0] and spindle[1] < range_end:
+				for truth in expert:
+					if overlap(truth, spindle):
+						c_precision += 1
+						break
+				t_precision += 1
+		precision = 0.0
+		if t_precision != 0:
+			precision = float(c_precision) / float(t_precision)
 
 		#recall
+		for truth in expert:
+			if range_start <= truth[0] and truth[1] < range_end:
+				for spindle in worker:
+					if overlap(truth, spindle):
+						c_recall += 1
+						break
+				t_recall += 1
 
-		cursor += stop
+		recall = 0.0
+		if t_recall != 0:
+			recall = float(c_recall) / float(t_recall)
+
+		#fscore
+		fscore = 0.0
+		if precision + recall > 0:
+			fscore = 2.0 * precision * recall / (precision + recall)
+
+		precisions.append(precision)
+		recalls.append(recall)
+		fscores.append(fscore)
+
+		cursor += step
 		x += 1
 		xs.append(x)
 
 	#plot
-	ax[index / 5][index % 5]
+	xs = xs[10:]
+	ys = fscores[10:]
+	zs = []
+	for y in ys:
+		zs.append(1.0 / (1.0 - float(y)))
+
+	slope, intercept, rvalue, pvalue, evalue = stats.linregress(xs, zs)
+
+	r = 1.0 / slope
+	p = (intercept - 1.0) * r
+
+
+	cs = []
+	for x in xs:
+		cs.append(float(x + p) / float(x + p + r))
+
+
+	a = index / 5
+	b = index % 5
+
+	ax[a][b].plot(xs, ys)
+	ax[a][b].plot(xs, cs)
 	index += 1
 
-
+plot.show()
 
 
 
