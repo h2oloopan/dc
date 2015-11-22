13252dc3addf7171e61fcabb7065c607bb845d84
Shengying Pan <i@thepans.me>
2015-11-22 08:51:30 -0500

    update

--- data/predict.py
+++ data/predict.py
@@ -50,14 +50,9 @@ for f in os.listdir('.'):
 f, ax = plot.subplots(2, 5)
 index = 0
 for worker in workers:
-	xs = []
-	precisions = []
-	recalls = []
-	fscores = []
 	cursor = 0
 	step = 20
 	end = 1080
-	x = 0
 
 
 	cumulative_precision = 0
@@ -70,15 +65,56 @@ for worker in workers:
 	cfs = []
 
 	#cumulative stuff
+	while cursor < end:
+		range_start = cursor
+		range_end = cursor + step
 
+		#precision
+		for spindle in worker:
+			if range_start <= spindle[0] and spindle[1] < range_end:
+				for truth in expert:
+					if overlap(truth, spindle):
+						cumulative_precision += 1
+						break
+				cumulative_precision_total += 1
+		precision = 0.0
+		if cumulative_precision_total != 0:
+			precision = float(cumulative_precision) / float(cumulative_precision_total)
 
+		#recall
+		for truth in expert:
+			if range_start <= truth[0] and truth[1] < range_end:
+				for spindle in worker:
+					if overlap(truth, spindle):
+						cumulative_recall += 1
+						break
+				cumulative_recall_total += 1
+
+		recall = 0.0
+		if cumulative_recall_total != 0:
+			recall = float(cumulative_recall) / float(cumulative_recall_total)
+
+		#fscore
+		fscore = 0.0
+		if precision + recall > 0:
+			fscore = 2.0 * precision * recall / (precision + recall)
+
+		cps.append(precision)
+		crs.append(recall)
+		cfs.append(fscore)
+
+		cursor += step
 
 	#actual stuff
+	cursor = 0
+	precisions = []
+	recalls = []
+	fscores = []
 
 
 	while cursor < end:
-		range_start = cursor - step
-		range_end = cursor + step + step
+		range_start = cursor - 2 * step
+		range_end = cursor + 3 * step
 
 		c_precision = 0
 		c_recall = 0
@@ -91,17 +127,11 @@ for worker in workers:
 				for truth in expert:
 					if overlap(truth, spindle):
 						c_precision += 1
-						cumulative_precision += 1
 						break
 				t_precision += 1
-				cumulative_recall_total += 1
 		precision = 0.0
 		if t_precision != 0:
 			precision = float(c_precision) / float(t_precision)
-
-		cp = 0.0
-		if cumulative_precision_total != 0:
-			cp = float(cumulative_precision) / float(cumulative_precision_total)
 
 		#recall
 		for truth in expert:
@@ -109,39 +139,28 @@ for worker in workers:
 				for spindle in worker:
 					if overlap(truth, spindle):
 						c_recall += 1
-						cumulative_recall += 1
 						break
 				t_recall += 1
-				cumulative_recall_total += 1
 
 		recall = 0.0
 		if t_recall != 0:
 			recall = float(c_recall) / float(t_recall)
-
-		cr = 0.0
-		if cumulative_recall_total != 0:
-			cr = float(cumulative_recall) / float(cumulative_recall_total)
 
 		#fscore
 		fscore = 0.0
 		if precision + recall > 0:
 			fscore = 2.0 * precision * recall / (precision + recall)
 
-		cf = 0.0
-		if cp + cr > 0:
-			cf = 2.0 * cp * cr / (cp + cr)
 
 		precisions.append(precision)
 		recalls.append(recall)
 		fscores.append(fscore)
 
-		cps.append(cp)
-		crs.append(cr)
-		cfs.append(cf)
 
 		cursor += step
-		x += 1
-		xs.append(x)
+
+
+	xs = np.arange(1, len(cfs) + 1, 1)
 
 	#plot
 	xs = xs[10:]
@@ -152,13 +171,15 @@ for worker in workers:
 	for y in ys:
 		zs.append(1.0 / (1.0 - float(y)))
 
-	slope, intercept, rvalue, pvalue, evalue = stats.linregress(xs[0:20], zs[0:20])
+	slope, intercept, rvalue, pvalue, evalue = stats.linregress(xs[0:30], zs[0:30])
 
 	r = 1.0 / slope
 	p = (intercept - 1.0) * r
 
 
 	print r, p, evalue, pvalue
+	print '---'
+
 
 	cs = []
 	for x in xs:
@@ -167,15 +188,30 @@ for worker in workers:
 		if x > 1:
 			q = float(x) * cq - float(x - 1) * (float(x - 1 + p) / float(x - 1 + p + r)) 
 		#cs.append(float(x + p) / float(x + p + r))
-		cs.append(q)
+		cs.append(cq)
 
+	print 'actual [45] ', cfs[44]
+	print 'average [45] ', cfs[39]
+	print 'projected [45] ', cs[24]
+
+	print 'actual [55]', cfs[-1]
+	print 'average [55]', cfs[39]
+	print 'projected [55] ', cs[-1]
+
+
+
+
+	print '---' 
 
 	a = index / 5
 	b = index % 5
 
-	ax[a][b].plot(xs, ys)
+	#ax[a][b].plot(xs, ys)
+	#ax[a][b].plot(xs, fscores[10:])
+	ax[a][b].plot(xs, cfs[10:])
 	ax[a][b].plot(xs, cs)
 	index += 1
+
 
 plot.show()
 
