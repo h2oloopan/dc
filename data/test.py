db07db9caa8eb02db69b7c83844b0e1b2234a956
Shengying Pan <i@thepans.me>
2015-12-02 09:21:25 -0500

    try to run real data

---
+++ data/test.py
@@ -0,0 +1,42 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plot
+import os
+import sys
+
+
+sys.path.append('../simulation')
+import learn
+from system import System
+
+expert = []
+workers = []
+
+def readData(fname):
+	result = []
+	with open(fname) as csvFile:
+		reader = csv.DictReader(csvFile)
+		for row in reader:
+			pair = [float(row['start']), float(row['end'])]
+			result.append(pair)
+	return result
+
+def overlap(p1, p2):
+	if p1[0] < p2[0] and p2[0] < p1[1]:
+		return True
+	elif p1[0] < p2[1] and p2[1] < p1[1]:
+		return True
+	elif p2[0] < p1[0] and p1[1] < p2[1]:
+		return True
+	else:
+		return False
+
+
+#do actual work
+for f in os.listdir('.'):
+	if f.endswith('.csv'):
+		if 'expert' not in f:
+			worker = readData(f)
+			workers.append(worker)
+		else:
+			expert = readData(f)
