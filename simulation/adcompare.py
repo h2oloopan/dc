877061379be11ceaa61cc43cfe0593cc71fca8b7
Shengying Pan <span@teebie.com>
2015-12-21 20:32:28 -0500

    update

--- simulation/adcompare.py
+++ simulation/adcompare.py
@@ -95,9 +95,13 @@ def analyze(graph, runs, steps, algorithm, tasks, outcomes, workers, parameters)
 
 	graph[0].set_xlabel('tasks')
 	graph[0].set_ylabel('accuracy')
+	graph[0].axis([0, 1000, 0.0, 1.0])
+	graph[0].set_autoscale_on(False)
 
 	graph[1].set_xlabel('tasks')
-	graph[1].set_ylabel('workers')
+	graph[1].set_ylabel('workers')	
+	graph[1].axis([0, 1000, 0.0, 5.0])
+	graph[1].set_autoscale_on(False)
 
 
 if __name__ == '__main__':
@@ -111,7 +115,7 @@ if __name__ == '__main__':
 	p = {'mu': 70, 'sigma': 10}
 	a = {'mu': 0.05, 'sigma': 0.04}
 
-	workers = simulate.createHyperbolicWorker(450, r, p, a, 1)
+	workers = simulate.createHyperbolicWorker(300, r, p, a, 1)
 
 	r2 = {'mu': 80, 'sigma': 10}
 	p2 = {'mu': 70, 'sigma': 10}
@@ -133,17 +137,17 @@ if __name__ == '__main__':
 	#workers.extend(available_workers)
 
 	#extra2 = simulate.createHyperbolicWorker(33, r2, p2, a2, 1)
-	extra3 = simulate.createHyperbolicWorker(50, r3, p3, a3, 1)
+	#extra3 = simulate.createHyperbolicWorker(50, r3, p3, a3, 1)
 	#extra4 = simulate.createHyperbolicWorker(33, r4, p4, a4, 1)
 
 	#workers.extend(extra2)
-	workers.extend(extra3)
+	#workers.extend(extra3)
 	#workers.extend(extra4)
 
 	tasks = simulate.createBinaryTasks(1000)
 	outcomes = [True, False]
-	runs = 1
-	steps = 3
+	runs = 5
+	steps = 4
 
 
 	f, ax = plot.subplots(3, 2)
@@ -151,13 +155,13 @@ if __name__ == '__main__':
 
 
 	print 'Random K'
-	k = 5
+	k = 3
 	analyze(ax[0], runs, steps, algorithm.randomK, tasks, outcomes, workers, k)
 
 
 
 	print 'Top K'
-	k = 5
+	k = 3
 	t = 20
 	analyze(ax[1], runs, steps, algorithm.topKAverageWithTutorials, tasks, outcomes, workers, [k, t])
 
@@ -166,7 +170,7 @@ if __name__ == '__main__':
 	##system = System(outcomes, 1000, {'belief' : 7.0, 'quality': 400.0})
 	system = System(outcomes, 1000, {'belief' : 7.0, 'quality': 60.0})
 	horizon = 5
-	samples = 1024
+	samples = 512
 	tutorials = 20
 	analyze(ax[2], runs, steps, system.dh, tasks, outcomes, workers, [horizon, samples, tutorials])
 
